0bf0fa9256921edde7202077042264b41b1f5ebb
allwhat <klinnkerry@gmail.com>
2022-07-06 15:35:07 -0500

    cuarto commit

--- allwhat/settings.py
+++ allwhat/settings.py
@@ -163,9 +163,9 @@ LEAFLET_CONFIG = {
 
 # Update database configuration with $DATABASE_URL.
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
