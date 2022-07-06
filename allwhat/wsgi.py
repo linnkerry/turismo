e514964133211f1b83346910ceb851648eeeb82c
allwhat <klinnkerry@gmail.com>
2022-07-06 17:23:15 -0500

    Decimo commit

--- allwhat/wsgi.py
+++ allwhat/wsgi.py
@@ -8,19 +8,9 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-import time
-import traceback
-import signal
-import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'allwhat.settings')
 
-try:
-    application = get_wsgi_application()
-except Exception:
-    if 'mod_wsgi' in sys.modules:
-        traceback.print_exc()
-        os.kill(os.getpid(), signal.SIGINT)
-        #time.sleep(2.5)
+application = get_wsgi_application()
