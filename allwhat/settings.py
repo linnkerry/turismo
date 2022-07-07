2872f44fa16bac5ea18aa813eb24d7a9aa81d48c
allwhat <klinnkerry@gmail.com>
2022-07-06 23:16:44 -0500

    Venticinco commit

--- allwhat/settings.py
+++ allwhat/settings.py
@@ -155,7 +155,7 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # STATICFILES_DIRS =(
 #     os.path.join(BASE_DIR,'static'),
